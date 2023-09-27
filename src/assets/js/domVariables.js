2cd7bc013c3e5217e0d561df74f6e3ebf8227e64
nicolettamerlodev <nicolettamerlodev@gmail.com>
2023-09-27 11:55:36 +0200

    created dist folder

--- src/assets/js/domVariables.js
+++
@@ -1,8 +0,0 @@
-export const outerChartLegend = document.getElementById("outerChartLegend");
-export const innerChartLegend = document.getElementById("innerChartLegend");
-export const ctx = document.getElementById("doughnutChart");
-
-export const malesAndFemalesInfoItem = document.querySelector('.malesAnFemales');
-export const femalesInfoItem = document.querySelector('.females');
-export const malesInfoItem = document.querySelector('.males');
-export const noData = document.querySelector('.noData');
